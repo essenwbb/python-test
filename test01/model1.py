6dc12f61edf29a100c37d4b0addb30eca8b82908
WuBinBin <1490779515@qq.com>
2019-03-19 22:21:45 +0800

    add main entrance

--- test01/model1.py
+++ test01/model1.py
@@ -5,3 +5,7 @@ class Model1(BaseModel):
     def init_birth(self):
         # Do something init _birth, eg: We only give it an initial value(my birth year).
         self._birth = 1994
+
+
+if __name__ == '__main__':
+    print(Model1()())
